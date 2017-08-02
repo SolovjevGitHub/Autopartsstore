74c7067578c453f1737983f8d0db25ba24f5e061
SolovjevGitHub <chester2010g@mail.ru>
2017-08-02 22:55:44 +0300

    v1

---
+++ autopartsstore-dao/src/main/java/com/intellisoft/autopartsstore/dao/impls/ICarMakeImpl.java
@@ -0,0 +1,12 @@
+package com.intellisoft.autopartsstore.dao.impls;
+
+import com.intellisoft.autopartsstore.dao.api.AbstarctDao;
+import com.intellisoft.autopartsstore.entitys.CarMake;
+
+/**
+ * Created by Сирожа и Маха on 02.08.2017.
+ */
+public class ICarMakeImpl extends AbstarctDao<CarMake> {
+    public ICarMakeImpl() {setClazz(CarMake.class);
+    }
+}
