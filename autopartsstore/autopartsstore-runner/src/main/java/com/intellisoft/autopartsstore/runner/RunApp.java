7e04ff831c13fbe4e99acf898b1e66d66eef7905
SolovjevGitHub <chester2010g@mail.ru>
2017-08-10 00:29:21 +0300

    new project-structure; interfaces for service-layer; impl for services;
    Signed-off-by: SolovjevGitHub <chester2010g@mail.ru>

--- autopartsstore/autopartsstore-runner/src/main/java/com/intellisoft/autopartsstore/runner/RunApp.java
+++ autopartsstore/autopartsstore-runner/src/main/java/com/intellisoft/autopartsstore/runner/RunApp.java
@@ -1,60 +1,19 @@
 package com.intellisoft.autopartsstore.runner;
 
-import com.intellisoft.autopartsstore.dao.entityimpls.CarModelDaoImpl;
-import com.intellisoft.autopartsstore.dao.hibernateutil.HibernateUtil;
-import com.intellisoft.autopartsstore.entitys.CarModel;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
+import com.intellisoft.autopartsstore.entity.AutopartBrand;
+import com.intellisoft.autopartsstore.entity.Order;
+import com.intellisoft.autopartsstore.service.impl.AutopartBrandServiceImpl;
+import com.intellisoft.autopartsstore.service.impl.OrderServiceImpl;
+
+import java.util.List;
 
 /**
  * Created by Сирожа и Маха on 01.08.2017.
  */
 public class RunApp {
     public static void main(String[] args) throws Exception {
-       // AutopartBrandServiceImpl autopartBrandService=new AutopartBrandServiceImpl();
-        //AutopartCategorieServiceImpl autopartCategorieService=new AutopartCategorieServiceImpl();
-        //AutopartServiceImpl autopartService=new AutopartServiceImpl();
-        //CarMakeServiceImpl carMakeService=new CarMakeServiceImpl();
-        //CarModelServiceImpl carModelService=new CarModelServiceImpl();
-        //OrderServiceImpl orderService=new OrderServiceImpl();
-        //UserRoleServiceImpl userRoleService=new UserRoleServiceImpl();
-        //UserServiceImpl userService=new UserServiceImpl();
-
-        /**CarMake*/
 
 
-        //CarMake carMake=new CarMake();
-        //System.out.println(carMakeService.getCarMakeByName("KIA").toString());
-
-                /**saveCarMake*/
-        //carMake.setName("VolksWagen");
-        //carMakeService.saveCarMake(carMake);
-                /**getCarMakeById*/
-        //carMake=carMakeService.getCarMakeById(3);
-        //System.out.println(carMake.toString());
-                /**deleteCarMake*/
-        //carMake=carMakeService.getCarMakeById(3);
-        //carMakeService.deleteCarMake(carMake);
-                /**updateCarMake*/
-        //carMake=carMakeService.getCarMakeById(15);
-        //carMake.setName("DAIHATSU");
-        //carMakeService.updateCarMake(carMake);
-                /**getAllCarMakes*/
-        //List<CarMake> carMakeList=carMakeService.getAllCarMakes();
-        //for(CarMake carMake:carMakeList){
-        // System.out.println(carMake.toString());        }
-
-        CarModelDaoImpl carModelDao=new CarModelDaoImpl();
-        Session session=HibernateUtil.getInstance().openSession();
-        Transaction transaction=session.getTransaction();
-        transaction.begin();
-        Criteria criteria = session.createCriteria(CarModel.class);
-        criteria.add(Restrictions.eq("bodyCode","a4b5"));
-        System.out.println(criteria.list().size());
-        transaction.commit();
-        session.close();
 
 
 
--- autopartsstore/autopartsstore-service/src/main/java/com/intellisoft/autopartsstore/service/com/intellisoft/autopartsstore/service/servicesapi/IAutopartCategorieService.java
+++
@@ -1,8 +0,0 @@
-package com.intellisoft.autopartsstore.service.com.intellisoft.autopartsstore.service.servicesapi;
-
-/**
- * Autoparts store project.
- * Author: Solovjev Sergey
- */
-public interface IAutopartCategorieService {
-}
